a673de5717795855a27b7ac9fbaf16127d6a549a
Sarthak Kathait <sarthak.btech@doonuniversity.ac.in>
2024-10-23 10:26:19 +0530

    Add files via upload

---
+++ default constructor.cpp
@@ -0,0 +1,20 @@
+#include iostream
+using namespace std;
+
+class A {
+public
+    int sum = 0; 
+    int n = 5;   
+
+    A() {
+        for (int i = 1; i = n; i++) { 
+            sum += i;
+        }
+        cout  Sum of the first   n   natural numbers is   sum  endl;
+    }
+};
+
+int main() {
+    A ob; 
+    return 0;
+}

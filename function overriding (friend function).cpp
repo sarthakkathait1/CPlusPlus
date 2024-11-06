d9e5f4d1b094d1ac81b40f91a764ab6875350420
Sarthak Kathait <sarthak.btech@doonuniversity.ac.in>
2024-11-06 10:42:12 +0530

    Add files via upload

---
+++ function overriding (friend function).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A{
+    private:
+    int a;
+    public:
+    A(int a){
+        this->a = a;
+    }
+    friend void operator ++(A& obj);
+    friend void operator -(A& obj);
+    friend void operator ~(A& obj);
+};
+    void operator ++(A& obj){
+        obj.a = obj.a+5;
+        cout << obj.a << endl;
+    }
+    void operator -(A &obj){
+        obj.a = obj.a-1;
+        cout << obj.a << endl;
+    }
+    void operator ~(A &obj){
+        obj.a = -(obj.a);
+        cout << obj.a;
+    }
+int main() {
+    A ob(8);
+    ++ob;
+    -ob;
+    ~ob;
+    return 0;
+}
